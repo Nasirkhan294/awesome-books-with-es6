b97b4a737daf5280f316f5e997fc74402324d64d
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-05-01 17:05:54 +0500

    Updated readme file with project details

---
+++ modules/date.js
@@ -0,0 +1,6 @@
+// import { DateTime } from 'luxon';
+// const date = document.querySelector('#date');
+
+// export default function checkDate() {
+//   date.innerHTML = DateTime.now().toLocaleString();
+// }
